f6009cd2dd1393155eb7ce3b0e779d7d7121aa18
Niklas Keller <me@kelunik.com>
2017-03-04 17:08:30 +0100

    Initial commit

---
+++ src/FqnChecker.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Kelunik\FqnCheck;
+
+use PhpParser\Node;
+use PhpParser\NodeVisitor;
+
+class FqnChecker implements NodeVisitor {
+    private $findings;
+
+    public function __construct() {
+        $this->reset();
+    }
+
+    private function reset() {
+        $this->findings = [];
+    }
+
+    public function beforeTraverse(array $nodes) {
+        $this->reset();
+    }
+
+    public function enterNode(Node $node) {
+        if (!$node instanceof Node\Expr\FuncCall) {
+            return;
+        }
+
+        if (!$node->name instanceof Node\Name) {
+            return;
+        }
+
+        if ($node->name->isFullyQualified()) {
+            return;
+        }
+
+        $this->findings[] = [
+            "line" => $node->getLine(),
+            "function" => (string) $node->name,
+        ];
+    }
+
+    public function leaveNode(Node $node) {
+        return $node;
+    }
+
+    public function afterTraverse(array $nodes) {
+        return $nodes;
+    }
+
+    public function getFindings() {
+        return $this->findings;
+    }
+}
